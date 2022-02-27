bab29b0a6a188b149c490db326f8ddf80219d574
meicilly <74332307+meicilly@users.noreply.github.com>
2022-02-27 15:46:52 +0800

    Update config.ts

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -19,9 +19,9 @@ const TIME_OUT = 10000
 if (process.env.NODE_ENV === 'development') {
   BASE_URL = '/api'
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = 'http://152.136.185.210:5000'
+  BASE_URL = '/api'
 } else {
-  BASE_URL = 'http://152.136.185.210:5000'
+  BASE_URL = '/api'
 }
 
 export { BASE_URL, TIME_OUT }
